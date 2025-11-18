fad39fa2db495d4bcbf7611cffad8fe9f4d3c3db
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-18 19:52:51 +0100

    chore(LiteDB): añadir LiteDB y testear

--- Backend/PluriConnectAPI/Program.cs
+++ Backend/PluriConnectAPI/Program.cs
@@ -1,6 +1,20 @@
+using LiteDB;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var db = new LiteDatabase(@"Data\pluriconnect.db");
+var children = db.GetCollection<Child>("children");
+
+app.MapGet("/", () => "Backend funcionando!");
+app.MapGet("/children", () => children.FindAll());
 
 app.Run();
+
+public class Child
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime Dob { get; set; }
+}
+
