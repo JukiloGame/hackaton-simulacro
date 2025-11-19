7ab524b0e4d4ad409fe757de26f70979b0802381
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-11-19 19:29:43 +0100

    feat(Data.Seed) Crear seed para inicializar datos de ejemplo con nuevo Model

--- Backend/PluriConnectAPI/Services/GenericService.cs
+++ Backend/PluriConnectAPI/Services/GenericService.cs
@@ -5,25 +5,24 @@ namespace PluriConnectAPI.Services;
 
 public class GenericService<T> where T : new()
 {
+
     private ILiteCollection<T> _col;
 
     public GenericService(LiteDatabase db)
     {
         // Nombre de la colección = nombre de la clase
-        _col = db.GetCollection<T>(typeof(T).Name);
+        _col = db.GetCollection<T>(GetCollectionName());
+    }
+        private static string GetCollectionName()
+    {
+        // Convierte "Child" -> "children", "Goal" -> "goals", etc.
+        return typeof(T).Name.ToLower() + "s";
     }
 
     public IEnumerable<T> GetAll() => _col.FindAll();
-
     public T? GetById(int id) => _col.FindById(id);
-
     public void Insert(T obj) => _col.Insert(obj);
-
     public bool Update(T obj) => _col.Update(obj);
-
-   public bool Delete(int id)
-    {
-        return _col.Delete(id);
-    }
+    public bool Delete(int id) { return _col.Delete(id); }
 
 }
